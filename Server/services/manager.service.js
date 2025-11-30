35a3676e13e25c009bb2be10cfee51336179fa05
MohammedG-AbdAllah <muhammed.abdallah@Student.giu-uni.de>
2025-11-30 09:58:15 +0200

    "Updated the implementation for the getCurrentUser in managerService"

--- Server/services/manager.service.js
+++ Server/services/manager.service.js
@@ -1,33 +1,38 @@
 const { acceptOrder } = require('../controllers/manager.controller');
 const order = require('../models/order');
 const Order = require('../models/order');
+const User = require('../models/user');
 const UserModel = require('../models/user');
 const { updateOrderStatus, getOrderById } = require('./order.service');
 const {acceptOrder} = require('./order.service');
+const customerService = require('./user.service');
+const {getCurrentUser} = require('./user.service');
 
 
 const managerService = {
 
     async getCurrentUser(id) {
 
-        if (!id) {
-            const error = new Error('No ID provided');
-            error.code = 400;
-            throw error;
-        }
-        const user = await UserModel.findById(id);
+        // if (!id) {
+        //     const error = new Error('No ID provided');
+        //     error.code = 400;
+        //     throw error;
+        // }
+        // const user = await UserModel.findById(id);
 
-        if (!user) {
-            const error = new Error('User not found');
-            error.code = 404;
-            throw error;
-        }
-        return {
-            id: user._id,
-            name: user.name,
-            email: user.email,
-            role: user.role
-        }
+        // if (!user) {
+        //     const error = new Error('User not found');
+        //     error.code = 404;
+        //     throw error;
+        // }
+        // return {
+        //     id: user._id,
+        //     name: user.name,
+        //     email: user.email,
+        //     role: user.role
+        // }
+
+        customerService.getCurrentUser(id);
     },
 
     async acceptOrder({orderID}){
