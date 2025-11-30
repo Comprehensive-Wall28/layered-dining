f15fffb772c8aab7a0de956eea820da9fb0cf22d
MohammedG-AbdAllah <muhammed.abdallah@Student.giu-uni.de>
2025-11-30 14:11:04 +0200

    "Started the implementation of routes for the manager.routes"

--- Server/routes/manager.routes.js
+++ Server/routes/manager.routes.js
@@ -4,3 +4,14 @@ const router = Router();
 const authenticationMiddleware = require('../middleware/authentication.middleware.js');
 const authorizationMiddleware = require('../middleware/authorization.middleware.js');
 const managerController = require("../controllers/manager.controller.js");
+
+router.get("/",authenticationMiddleware,userController.getCurrentUser);
+//router.get("/cart", authenticationMiddleware, userController.getCartId);
+router.put("/profile", authenticationMiddleware, userController.updateUserProfile)
+router.get("/log/:id", authenticationMiddleware, authorizationMiddleware([ROLES.ADMIN]), userController.getLogs)
+
+
+router.put("/order/:id", authenticationMiddleware, authorizationMiddleware([ROLES.MANAGER]),managerController.updateOrderStatus)
+router.put("/order/:id", authenticationMiddleware, authorizationMiddleware([ROLES.MANAGER]), managerController.acceptOrder)
+
+//router.delete("/delete/:id",authenticationMiddleware,authorizationMiddleware([ROLES.CUSTOMER]),userController.deleteAccount);
